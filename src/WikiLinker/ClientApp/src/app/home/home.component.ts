900decc22ad9044b4256e127d7893c7be028071d
Ievgen-Zubkov <ievgen_zubkov@epam.com>
2019-10-08 16:53:16 +0300

    Add photos

--- src/WikiLinker/ClientApp/src/app/home/home.component.ts
+++ src/WikiLinker/ClientApp/src/app/home/home.component.ts
@@ -9,13 +9,25 @@ import { HttpClient } from '@angular/common/http';
 export class HomeComponent {
   article = <Article>{};
   wikiLinkedArticle = <Article>{};
+  entityTypes = [];
+  photos = [];
   articleForm = this.formBuilder.group({
     text: new FormControl(this.article.text, [Validators.required, Validators.minLength(3)])
   });
 
   onSubmit(articleData) {
     this.http.post(this.baseUrl + 'api/articles', articleData).subscribe(
-      result => this.wikiLinkedArticle = <Article>{ text: (<any>result).input },
+      result => {
+        this.wikiLinkedArticle = <Article>{ text: (<any>result).input };
+        this.photos = (<any>result).photos;
+        for (var i = 0; i < this.photos.length; i++) {
+          var typeName = this.photos[i].type;
+          var type = { name: typeName, photos: this.photos.filter(p => p.type == typeName) };
+          if (this.entityTypes.filter(e => e.name == typeName).length == 0) {
+            this.entityTypes.push(type);
+          }
+        }
+      },
       error => console.error(error)
     );
   }
