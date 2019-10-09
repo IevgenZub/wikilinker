116e77608bae297b3025856643e83a11a891efe5
Ievgen-Zubkov <ievgen_zubkov@epam.com>
2019-10-09 10:31:39 +0300

    Code clean up

--- src/WikiLinker/Services/WikiLinkerService.cs
+++ src/WikiLinker/Services/WikiLinkerService.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.Azure.CognitiveServices.Language.TextAnalytics;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
